69dcf01b5e01c018d4ab27d92b1a0dd4ca191f43
AdamKelly <adam.obare@student.moringaschool.com>
2023-08-25 11:51:55 +0300

    Done

--- lib/student.py
+++ lib/student.py
@@ -5,4 +5,10 @@ from user import User
 class Student(User):
     
     def learn(self):
-        pass
+     pass
+     def __init__(self, first_name, last_name='cynthia'):
+      super().__init__(first_name, last_name)
+     self.knowledge = []
+
+    def learn(self, new_knowledge):
+        self.knowledge.append(new_knowledge)
